686a6e5ad157ff9e40f7c8bf02f137c28d9b65eb
engtasneem <134233991+engtasneem@users.noreply.github.com>
2023-05-22 20:45:01 +0300

    Add files via upload

---
+++ ConsoleApplication1/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Employee
+    {
+        private string name;
+        private string address;
+        private int salary;
+        public Employee()
+        {
+            Console.WriteLine("wlecome to our conpany");
+            this.name = "Unkown";
+            this.address = "Mukalla";
+            this.salary = 3000;
+        }
+
+
+        public Employee (string name):this()
+        {
+            this.name = name;
+            Console.WriteLine(name);
+        }
+        public Employee (string name,string address):this(name)
+        {
+            this.address = address;
+           Console.WriteLine(address);
+        }
+        public Employee(string name, string address,int salary):this(name,address)
+       {
+            this.salary = salary;
+            Console.WriteLine(salary);
+        }
+       
+       
+
+
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e = new Employee("ali");
+            Employee e1 = new Employee("ahamed", "Aden");
+            Employee e2 = new Employee("noor", "yemen", 222);
+        }
+    }
+}
